4e21c9c5be3994274f050ea2588573ca35ffd4a6
alfheiduredda <40697429+alfheiduredda@users.noreply.github.com>
2019-03-18 22:58:43 +0000

    Add files via upload

--- motionControl.py
+++ motionControl.py
@@ -2,32 +2,98 @@ import numpy as np
 import numpy.linalg as lin
 import matplotlib.pyplot as plt
 
-def Trapezoid(f,a,b,m):
-    h = (b-a)/m
-    Grid = np.arange(a,b+h,h)
-    intf = -h*(f(a)+f(b))/2
-    for xi in Grid:
-        intf += h*f(xi)
-    return intf
+
+def Trap(f, a, b):
+    return (f(a)+f(b))*(b-a)/2
+
+
+def lengthen(v):
+    m = len(v)
+    newv = np.zeros(2*m)
+    for i in range(m):
+        newv[i]=v[i]
+    return newv
+
 
 def adaptiveQuadrature(f, a0, b0, tol0):
     sum = 0
     n = 1
-    a = [a0]
-    b = [b0]
-    tol = [tol0]
-    app = [Trapezoid(f,a,b,1)]
+    a = np.zeros(1000)
+    b = np.zeros(1000)
+    app = np.zeros(1000)
+    tol = np.zeros(1000)
+    a[0] = a0
+    b[0] = b0
+    tol[0] = tol0
+    app[0] = Trap(f, a0, b0)
     while n > 0:
+        if len(app) < 2*n:
+            app = lengthen(app)
+            a = lengthen(a)
+            b = lengthen(b)
+            tol = lengthen(tol)
         c = (a[n-1] + b[n-1])/2
-        oldapp = app(n-1)
-        app(n) =
-    return 5
+        oldapp = app[n-1]
+        app[n-1] = Trap(f, a[n-1], c)
+        app[n] = Trap(f, c, b[n-1])
+        if np.abs(oldapp-app[n-1]-app[n]) < 3*tol[n-1]:
+            sum += app[n-1]+app[n]
+            n -= 1
+        else:
+            b[n] = b[n-1]
+            b[n-1] = c
+            a[n] = c
+            tol[n-1] /=2
+            tol[n] = tol[n-1]
+            n += 1
+    return sum
 
+
+def binary(f, a, b, tol):
+    while (b-a)/2 > tol:
+        c = (a+b)/2
+        if f(c) == 0:
+            return c
+        if f(a)*f(c) < 0:
+            b = c
+        else:
+            a = c
+    return c
+
+def Newton(f,df,x0,tol):
+    x = x0
+    while np.abs(f(x)) > tol:
+        x -= f(x)/df(x)
+    return x
+    
+    
+
+def tstar(arc, s, tollength, tolbinary):
+    l = adaptiveQuadrature(arc, 0, 1, tollength)
+    Arc = lambda t: adaptiveQuadrature(arc,0,t,tollength)-l*s
+    return binary(Arc, 0, 1, tolbinary)
+
+def tstar2(arc, s, tollength, tolNewton):
+    l = adaptiveQuadrature(arc, 0, 1, tollength)
+    Arc = lambda t: adaptiveQuadrature(arc,0,t,tollength)-l*s
+    return Newton(Arc,arc, s, tolNewton)
+    
 
 x = lambda t: 0.5 + 0.3*t + 3.9*t**2 - 4.7*t**3
 y = lambda t: 0.5 + 0.3*t + 0.9*t**2 - 2.7*t**3
 dx = lambda t: 0.3 + 7.8*t - 14.1*t**2
 dy = lambda t: 0.3 + 1.8*t - 8.1*t**2
 arc = lambda t: np.sqrt(dx(t)**2 + dy(t)**2)
-val = adaptiveQuadrature(arc,0,0.5,0.0001)
+f = lambda t: t**2
+val = adaptiveQuadrature(arc,0,0.2, 0.000001)
 print(val)
+l = adaptiveQuadrature(arc, 0, 1, 0.00001)
+s = 0.7
+tstarr = tstar(arc, s, 0.000001, 0.000001)
+tstarr2 = tstar2(arc, s, 0.0001, 0.0001)
+print(tstarr)
+print(tstarr2)
+print(s*l)
+print(adaptiveQuadrature(arc,0, tstarr, 0.0001))
+print(adaptiveQuadrature(arc,0, tstarr2, 0.0001))
+
